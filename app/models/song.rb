d4c389708545582025dd1d5d7cbdb15caf0069c1
Caroline Shields <cbshields@outlook.com>
2018-04-17 00:29:05 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    
   end
 
   def artist_name=(name)
     artst = Artist.find_by_or_create(name: name)
-    self.artist.artst
+    self.artist = artst
   end
 end
